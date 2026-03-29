3831c5222e440f3b8cd146c0a996ce51eb99fe0a
nouioua150 <nouiouanouioua51@gmail.com>
2026-03-30 00:10:39 +0100

    Update Dichotomie.py
    تغيير في الرموز فقط

--- Unit1_Roots/Dichotomie.py
+++ Unit1_Roots/Dichotomie.py
@@ -2,15 +2,15 @@ def f(x):
     # تعريف الدالة: يمكنك تغييرها حسب المسألة
     return x**2 - 2 
 
-def bisection_method(a, b, tol, max_iter):
+def bisection_method(a, b, E, N ):
     # التحقق من شرط الإشارة
-    if f(a) * f(b) >= 0:
-        print("f(a) * f(b) must be negative")
+    if f(a) * f(b) > 0:
+        print(" لا يوجد حل في هذا المجال")
         return None
 
     print(f"{'Iter':<10} | {'x':<15} | {'Error':<15}")
     
-    for i in range(1, max_iter + 1):
+    for i in range(1, N + 1):
         c = (a + b) / 2
         error = abs(b - a) / 2
         
@@ -27,7 +27,7 @@ def bisection_method(a, b, tol, max_iter):
         else:
             a = c
             
-            # التعديل هنا: إذا خرجنا من الحلقة بلا ما يتحقق شرط error < tol
+            # التعديل هنا: إذا خرجنا من الحلقة بلا ما يتحقق شرط error < E
     print("\nتنبيه: تم الوصول للحد الأقصى من الدورات دون تحقيق الدقة المطلوبة.")
     return c
 
